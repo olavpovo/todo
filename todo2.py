cc0235153f5850c08efd31526f3fef787da769d3
Olav Povo <olavpovo@gmail.com>
2026-05-30 22:57:47 -0400

    todo2.py- added app framework

--- todo2.py
+++ todo2.py
@@ -1,2 +1,29 @@
 ##Todo app##
 
+## Create a tasklist ##
+
+## Each task is added to the tasklist ##
+
+## All functions must return variables that will be parsed by client-facing app. ie, Django, GUI
+
+
+# Tasklist-Specific Ops
+# Add
+# Edit
+# Delete
+# Run function
+# Load Tasklist
+# Close
+
+
+# Task-Specific Ops
+
+# Each task has attributes- index, name, group, date added, frequency, interval
+# Task attributes:
+# index
+# name
+# dateadded
+# frequency
+# completed=[]
+# interval
+# group/category
