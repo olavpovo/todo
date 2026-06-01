8fc11969961a9f026b7e6e4f2f42510023ae41b5
Olav Povo <olavpovo@gmail.com>
2026-06-01 16:18:00 -0400

    added more detail to app operation

--- todo2.py
+++ todo2.py
@@ -1,29 +1,33 @@
 ##Todo app##
 
+## Operation
+
 ## Create a tasklist ##
-
 ## Each task is added to the tasklist ##
-
+## App can be UI facing or web-facing
+## Will store entries into a database
 ## All functions must return variables that will be parsed by client-facing app. ie, Django, GUI
 
 
+
 # Tasklist-Specific Ops
-# Add
-# Edit
-# Delete
-# Run function
-# Load Tasklist
-# Close
+# Add-        will add tasks into the main tasklist
+# Edit-       edit a task in tasklist
+# Delete-     delete, in tasklist
+# Run-        run todo main function
+# Load-       load entries into tasklist
+# Close-      save all changes and exit
 
 
 # Task-Specific Ops
 
 # Each task has attributes- index, name, group, date added, frequency, interval
 # Task attributes:
-# index
-# name
-# dateadded
-# frequency
-# completed=[]
-# interval
+# index:    # of task in tasklist
+# name      name of task
+# added     date added
+# frequency how often does the task repeat- D,W,BW,M,Y,I
+# interval  numerical value of frequency
+
 # group/category
+# completed=[]
